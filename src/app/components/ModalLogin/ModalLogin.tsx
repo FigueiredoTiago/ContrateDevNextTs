ee7470b62f83d47f27439f227556402c57b4bf4b
TFscript <developertiagof@gmail.com>
2025-06-22 23:03:34 -0300

    Mudanca no layout do app

--- src/app/components/ModalLogin/ModalLogin.tsx
+++ src/app/components/ModalLogin/ModalLogin.tsx
@@ -1,3 +1,4 @@
+"use client";
 import styles from "./styles.module.css";
 import LoginBtn from "../loginBtn/LoginBtn";
 import Image from "next/image";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 // import ModalLogin from "./components/ModalLogin/ModalLogin";
 "use client";
-import Navbar from "./components/navbar/Navbar";
+
 import Dashboard from "./dashboard/Dashboard";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      
       <Dashboard/>
       
     </>
