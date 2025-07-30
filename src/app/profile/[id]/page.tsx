bd37fe071a7b8d1655b088dbb7f253acae666249
TFscript <developertiagof@gmail.com>
2025-07-30 09:13:38 -0300

    styles: fixx na sider view do profile

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -10,6 +10,10 @@ type Props = {
 const ProfilePage = ({ params }: Props) => {
   return (
     <section className={styles.container}>
+
+      <h1 className={styles.title}> <a href="/">contrate.dev</a> apresenta:
+Um candidato preparado para agregar valor à sua equipe. Analise o perfil, avalie a experiência e faça contato para avançar na contratação.  </h1>
+
       <ProfileView id={params.id} />
     </section>
   );
